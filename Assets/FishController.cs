06ccd37f419d243ecfbbf576aa96631b4f105c2a
jseymour93 <seymour.jordan93@gmail.com>
2024-03-08 18:34:42 -0600

    lefeesh swims

--- Assets/FishController.cs
+++ Assets/FishController.cs
@@ -1,48 +1,62 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class FishController : MonoBehaviour
 {
-    public float speed = 1.0f;
-    public LayerMask waterLayer;
-    public float detectionRadius = 5.0f;
-    private Vector3 targetPosition;
+    public float minSpeed = 1f;
+    public float maxSpeed = 5f;
+    public Tilemap waterTilemap;
+    private float speed;
+    private Vector2 direction;
+    private Color originalColor;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        ChooseNewTarget();
+        StartCoroutine(ChangeDirection());
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Color color = spriteRenderer.color;
+        originalColor = color;
+        color.a = 0.5f;
+        spriteRenderer.color = color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Move towards the target
-        Vector3 direction = targetPosition - transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-
-        // Rotate to face the direction of movement
-        if (direction != Vector3.zero)
+        Vector2 offset = direction * 0.5f; // Change this value to adjust the offset
+        Vector2 newPosition = (Vector2)transform.position + direction * speed * Time.deltaTime + offset;
+        Vector3Int tilePosition = waterTilemap.WorldToCell(newPosition);
+        if (waterTilemap.HasTile(tilePosition))
         {
+            transform.position = newPosition - offset;
+
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
-
-        // If we've reached the target, choose a new one
-        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
-        {
-            ChooseNewTarget();
-        }
     }
 
-    void ChooseNewTarget()
+    private IEnumerator ChangeDirection()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius, waterLayer);
-        if (hitColliders.Length > 0)
+        while (true)
         {
-            int randomIndex = Random.Range(0, hitColliders.Length);
-            targetPosition = hitColliders[randomIndex].transform.position;
+            speed = Random.Range(minSpeed, maxSpeed);
+            direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+
+            Vector2 newPosition = (Vector2)transform.position + direction * speed * Time.deltaTime;
+            Vector3Int tilePosition = waterTilemap.WorldToCell(newPosition);
+            if (!waterTilemap.HasTile(tilePosition))
+            {
+                continue;
+            }
+
+            yield return new WaitForSeconds(Random.Range(1f, 5f));
         }
+    }
+    public void CatchFish()
+    {
+        StopAllCoroutines();
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = originalColor;
+        Destroy(gameObject);
     }
 }
